7ccfe19cbf122783122d00f4fe01cc514d34d44a
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c2r1p3.42sp.org.br>
2025-07-19 16:58:10 -0300

    base for development

--- get_next_line.test.c
+++ get_next_line.test.c
@@ -6,7 +6,7 @@
 /*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 16:55:54 by jtertuli          #+#    #+#             */
-/*   Updated: 2025/07/19 16:56:23 by jtertuli         ###   ########.fr       */
+/*   Updated: 2025/07/19 16:58:00 by jtertuli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 MU_TEST(test_check) {
 	mu_check(5 == 5);
 }
+
 MU_TEST_SUITE(test_suite) {
 	MU_RUN_TEST(test_check);
 }
