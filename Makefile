ce57d970e2da5a1090207cae2afe405d8980e147
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c2r1p3.42sp.org.br>
2025-07-19 16:57:46 -0300

    base for development

---
+++ Makefile
@@ -0,0 +1,30 @@
+NAME = gnl
+
+SRC = get_next_line.c
+SRC_TEST = get_next_line.test.c
+
+CC = gcc
+FLAGS = -Wall -Werror -Wextra -g
+
+OBJ_SRC = $(SRC:.c=.o)
+OBJ_SRC_TEST = $(SRC_TEST:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ_SRC) $(OBJ_SRC_TEST)
+	$(CC) $(FLAGS) $(OBJ_SRC) $(OBJ_SRC_TEST) -o $(NAME)
+	make clean
+
+%.o: %.c
+	$(CC) $(FLAGS) -c $< -o $@
+
+test: all 
+	./$(NAME)
+
+clean:
+	rm -f $(OBJ_SRC) $(OBJ_SRC_TEST)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
---
+++ get_next_line.test.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.test.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 16:55:54 by jtertuli          #+#    #+#             */
+/*   Updated: 2025/07/19 16:56:23 by jtertuli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minunit.h"
+
+MU_TEST(test_check) {
+	mu_check(5 == 5);
+}
+MU_TEST_SUITE(test_suite) {
+	MU_RUN_TEST(test_check);
+}
+
+int	main(void)
+{
+	MU_RUN_SUITE(test_suite);
+	MU_REPORT();
+	return (MU_EXIT_CODE);
+}
