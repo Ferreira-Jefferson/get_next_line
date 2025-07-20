c64f5e3d9b5c5ed21819306c5f491ad07b1737a8
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c1r1p4.42sp.org.br>
2025-07-20 07:57:26 -0300

    refact: norminette

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,15 @@
 /*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 15:24:22 by jtertuli          #+#    #+#             */
-/*   Updated: 2025/07/20 07:22:12 by jtertuli         ###   ########.fr       */
+/*   Updated: 2025/07/20 07:56:22 by jtertuli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
